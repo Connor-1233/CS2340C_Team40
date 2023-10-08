e90d7ffa44822903d37b88d68cc63fef5a840047
Joshua Mitchell <joshuamit2003@gmail.com>
2023-10-08 17:18:23 -0400

    Added leaderboard backend

--- app/src/main/java/com/example/cs2340c_team40/Model/Leaderboard.java
+++ app/src/main/java/com/example/cs2340c_team40/Model/Leaderboard.java
@@ -1,4 +1,39 @@
 package com.example.cs2340c_team40.Model;
+import java.util.ArrayList;
 
 public class Leaderboard {
+    private static Leaderboard gameLeaderBoard;
+    private static ArrayList<Score> rankings;
+    private int lowestRankedScore;
+    private Leaderboard() {
+        ArrayList<Score> rankings = new ArrayList<Score>();
+    }
+
+    //Updates and sorts the score list with the new score
+    public void updateScore(int score, String name) {
+        if (rankings.size() >= 5 & score < lowestRankedScore) {
+            return;
+        }
+        Score newScore = new Score(score, name);
+        for (int i = 0; i < 5; i++) {
+            Score currScore = rankings.get(i);
+            if (score > currScore.getScore()) {
+                rankings.add(i, newScore);
+                return;
+            }
+        }
+    }
+
+    //Method to grab a certain score from the list
+    public Score getScore(int index) {
+        return rankings.get(index);
+    }
+
+    //Singleton method to grab one instance of the leaderboard
+    public static Leaderboard getInstance() {
+        if (gameLeaderBoard == null) {
+            gameLeaderBoard = new Leaderboard();
+        }
+        return gameLeaderBoard;
+    }
 }
