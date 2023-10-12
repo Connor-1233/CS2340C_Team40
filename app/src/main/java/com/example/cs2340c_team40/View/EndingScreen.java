dd792fd690fb06a834537179530e2c7c280921e8
jthomas462 <jonathanthom145@gmail.com>
2023-10-12 11:14:07 -0400

    Leaderboard changes

--- app/src/main/java/com/example/cs2340c_team40/View/EndingScreen.java
+++ app/src/main/java/com/example/cs2340c_team40/View/EndingScreen.java
@@ -31,9 +31,75 @@ public class EndingScreen extends Activity {
         gameScore.setText("Final score: " + player.getScore());
         leaderboard.updateScore(player.getScore(), player.getName());
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new LeaderboardAdapter(getApplicationContext()));
+        // Leaderboard
+        // Rank 1
+        TextView score_name_1 = findViewById(R.id.score_name_1);
+        score_name_1.setText((leaderboard.getScore(0) != null)
+                                ? leaderboard.getScore(0).getName()
+                                : "More Attempts Needed");
+        TextView score_score_1 = findViewById(R.id.score_score_1);
+        score_score_1.setText((leaderboard.getScore(0) != null)
+                                ? "" + leaderboard.getScore(0).getScore()
+                                : "More Attempts Needed");
+        TextView score_date_1 = findViewById(R.id.score_date_1);
+        score_date_1.setText((leaderboard.getScore(0) != null)
+                                ? leaderboard.getScore(0).getTime()
+                                : "More Attempts Needed");
+        // Rank 2
+        TextView score_name_2 = findViewById(R.id.score_name_2);
+        score_name_2.setText((leaderboard.getScore(1) != null)
+                ? leaderboard.getScore(1).getName()
+                : "More Attempts Needed");
+        TextView score_score_2 = findViewById(R.id.score_score_2);
+        score_score_2.setText((leaderboard.getScore(1) != null)
+                ? "" + leaderboard.getScore(1).getScore()
+                : "More Attempts Needed");
+        TextView score_date_2 = findViewById(R.id.score_date_2);
+        score_date_2.setText((leaderboard.getScore(1) != null)
+                ? leaderboard.getScore(1).getTime()
+                : "More Attempts Needed");
+        // Rank 3
+        TextView score_name_3 = findViewById(R.id.score_name_3);
+        score_name_3.setText((leaderboard.getScore(2) != null)
+                ? leaderboard.getScore(2).getName()
+                : "More Attempts Needed");
+        TextView score_score_3 = findViewById(R.id.score_score_3);
+        score_score_3.setText((leaderboard.getScore(2) != null)
+                ? "" + leaderboard.getScore(2).getScore()
+                : "More Attempts Needed");
+        TextView score_date_3 = findViewById(R.id.score_date_3);
+        score_date_3.setText((leaderboard.getScore(2) != null)
+                ? leaderboard.getScore(2).getTime()
+                : "More Attempts Needed");
+        // Rank 4
+        TextView score_name_4 = findViewById(R.id.score_name_4);
+        score_name_4.setText((leaderboard.getScore(3) != null)
+                ? leaderboard.getScore(3).getName()
+                : "More Attempts Needed");
+        TextView score_score_4 = findViewById(R.id.score_score_4);
+        score_score_4.setText((leaderboard.getScore(3) != null)
+                ? "" + leaderboard.getScore(3).getScore()
+                : "More Attempts Needed");
+        TextView score_date_4 = findViewById(R.id.score_date_4);
+        score_date_4.setText((leaderboard.getScore(3) != null)
+                ? leaderboard.getScore(3).getTime()
+                : "More Attempts Needed");
+        // Rank 5
+        TextView score_name_5 = findViewById(R.id.score_name_5);
+        score_name_5.setText((leaderboard.getScore(4) != null)
+                ? leaderboard.getScore(4).getName()
+                : "More Attempts Needed");
+        TextView score_score_5 = findViewById(R.id.score_score_5);
+        score_score_5.setText((leaderboard.getScore(4) != null)
+                ? "" + leaderboard.getScore(4).getScore()
+                : "More Attempts Needed");
+        TextView score_date_5 = findViewById(R.id.score_date_5);
+        score_date_5.setText((leaderboard.getScore(4) != null)
+                ? leaderboard.getScore(4).getTime()
+                : "More Attempts Needed");
+//        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(new LeaderboardAdapter(getApplicationContext()));
     }
 
     private void restartGame() {
