87338d4426c51112fed2cc92a3723f34915eac5a
Joshua Mitchell <joshuamit2003@gmail.com>
2023-10-23 23:45:02 -0400

    Adding tests

--- app/src/test/java/com/example/cs2340c_team40/ObserverUnitTests.java
+++ app/src/test/java/com/example/cs2340c_team40/ObserverUnitTests.java
@@ -4,8 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.widget.ImageView;
+
+import com.example.cs2340c_team40.Model.MoveHorizontal;
+import com.example.cs2340c_team40.Model.MoveVertical;
 import com.example.cs2340c_team40.Model.Player;
 import com.example.cs2340c_team40.Model.Subscriber;
+
+
+
+import java.util.ArrayList;
 
 public class ObserverUnitTests {
 
@@ -20,9 +28,22 @@ public class ObserverUnitTests {
     public void testUpdate() {
         Player p1 = Player.getInstance();
         Player p2 = Player.getInstance();
-        p1.update();
         assertEquals(p1.getX(), p2.getX());
         assertEquals(p2.getY(), p1.getY());
     }
 
+    @Test
+    public void ObserverTest() {
+        Player player = Player.getInstance();
+        player.setX(0);
+        player.setY(0);
+        MoveHorizontal dir = new MoveHorizontal(1);
+        player.setMoveDirection(dir);
+        dir.movePlayer();
+        MoveVertical dirVer = new MoveVertical(1);
+        player.setMoveDirection(dirVer);
+        dirVer.movePlayer();
+        assertEquals(player.getX(), 5);
+        assertEquals(player.getY(), 5);
+    }
 }
---
+++ app/src/test/java/com/example/cs2340c_team40/StrategyUnitTests.java
@@ -0,0 +1,26 @@
+package com.example.cs2340c_team40;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.cs2340c_team40.Model.MoveHorizontal;
+import com.example.cs2340c_team40.Model.Player;
+import com.example.cs2340c_team40.Model.PlayerDirection;
+import com.example.cs2340c_team40.Model.Room;
+import com.example.cs2340c_team40.Model.Subscriber;
+import com.example.cs2340c_team40.ViewModel.GameScreenViewModel;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class StrategyUnitTests {
+    @Test
+    public void StrategyTest() {
+        Player p = Player.getInstance();
+        GameScreenViewModel.initializePlayer(5,0,new Room(), new ArrayList<Subscriber>());
+        PlayerDirection dir = new MoveHorizontal(-1);
+        p.setMoveDirection(dir);
+        dir.movePlayer();
+        assertEquals(p.getX(), 0);
+    }
+}
