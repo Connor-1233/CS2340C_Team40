bc66b18b0c18987aa896c37ce1578e3f7c4d12c8
adhishrajan <75100186+adhishrajan@users.noreply.github.com>
2023-11-12 23:29:34 -0500

    Update LeaderboardUnitTests.java
    add junits (adhish)

--- app/src/test/java/com/example/cs2340c_team40/LeaderboardUnitTests.java
+++ app/src/test/java/com/example/cs2340c_team40/LeaderboardUnitTests.java
@@ -10,10 +10,9 @@ public class LeaderboardUnitTests {
     @Test
     public void AddScoreToLeaderboard() {
         Leaderboard testBoard = Leaderboard.getInstance();
-        testBoard.resetLeaderboard();
         //Add a first score
         testBoard.updateScore(70, "James");
-        assertEquals(70, testBoard.getScore(0).getScore());
+        assertEquals(testBoard.getScore(0).getScore(), 70);
         assertEquals(testBoard.getScore(0).getName(), "James");
         //Add a second score
         testBoard.updateScore(60, "John");
@@ -43,29 +42,5 @@ public class LeaderboardUnitTests {
         assertEquals(testBoard.getScore(1).getScore(), 50);
         assertEquals(testBoard.getScore(2).getScore(), 40);
         assertEquals(testBoard.getScore(3).getScore(), 30);
-    }
-
-    @Test
-    public void isSingleton() {
-        
-        Leaderboard board1 = Leaderboard.getInstance();
-        board1.updateScore(10, "A");
-        Leaderboard board2 = Leaderboard.getInstance();
-        board2.updateScore(20, "B");
-        Leaderboard board3 = Leaderboard.getInstance();
-        board3.updateScore(30, "C");
-        Leaderboard board4 = Leaderboard.getInstance();
-        board4.updateScore(40, "D");
-
-        assertEquals(board1.getSize(), board2.getSize());
-        assertEquals(board3.getSize(), board4.getSize());
-        assertEquals(board1.getSize(), board4.getSize());
-
-        for (int i = 0; i < board1.getSize(); i++) {
-            assertEquals(board1.getScore(i).getName(), board2.getScore(i).getName());
-            assertEquals(board3.getScore(i).getName(), board4.getScore(i).getName());
-            assertEquals(board1.getScore(i).getName(), board4.getScore(i).getName());
-        }
-        
     }
 }
