d96670a0b4b290cdc0fe3598f44f5ac2af89ce84
Joshua Mitchell <joshuamit2003@gmail.com>
2023-11-28 21:12:59 -0500

    Adding unit tests

--- app/src/test/java/com/example/cs2340c_team40/WeaponUnitTests.java
+++ app/src/test/java/com/example/cs2340c_team40/WeaponUnitTests.java
@@ -3,13 +3,52 @@ package com.example.cs2340c_team40;
 import static org.junit.Assert.assertEquals;
 
 
+import com.example.cs2340c_team40.Model.Enemy;
+import com.example.cs2340c_team40.Model.EnemyFactory;
+import com.example.cs2340c_team40.Model.MoveHorizontal;
 import com.example.cs2340c_team40.Model.Player;
+import com.example.cs2340c_team40.Model.PlayerDirection;
 import com.example.cs2340c_team40.Model.Weapon;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class WeaponUnitTests {
+
+    @Test
+    public void testWeaponNoDirection() {
+        EnemyFactory enemyCreator = new EnemyFactory();
+        Weapon w = Weapon.getInstance();
+        Player p = Player.getInstance();
+        Enemy e = enemyCreator.createEnemy("Ghost");
+        p.getEnemyList().add(e);
+        p.setX(100);
+        p.setY(100);
+        e.setX(180);
+        e.setY(100);
+        w.notifyEnemies();
+        assertEquals(false, e.isEnemyDestroyed());
+    }
+
+    @Test
+    public void testWeaponDirection() {
+        EnemyFactory enemyCreator = new EnemyFactory();
+        Weapon w = Weapon.getInstance();
+        Player p = Player.getInstance();
+        Enemy e = enemyCreator.createEnemy("Ghost");
+        p.getEnemyList().add(e);
+        p.setX(100);
+        p.setY(100);
+        e.setX(190);
+        e.setY(100);
+        PlayerDirection mh = new MoveHorizontal(1);
+        p.setMoveDirection(mh);
+        p.setSpeed(10);
+        mh.movePlayer();
+        w.notifyEnemies();
+        assertEquals('.', p.getDirection());
+        assertEquals(110, p.getX());
+    }
 
     @Before
     public void setUp() {
